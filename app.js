3a61890da14d837d7e230411d91a7435a1e1e81c
Vladislav Mihov <vlad.slav.vo.wo@gmail.com>
2021-01-03 21:31:37 +0200

    Add navbar.

---
+++ app.js
@@ -0,0 +1,32 @@
+const navLinksFade = () => {
+    const navLinks = document.querySelectorAll('.nav-links li');
+    
+    navLinks.forEach((link, index) => {
+        if (link.style.animation) {
+            link.style.animation = '';
+        }
+        else {
+            console.log(index);
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + 0.5}s`;
+        }
+    });
+}
+
+
+const navToggle = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+        navLinksFade();
+        // Burger animation
+        burger.classList.toggle('toggle');
+    });
+}
+
+const app = () => {
+    navToggle();
+}
+
+app();
