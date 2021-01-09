cca8efb736588a3dac00508420342ca88a972ea8
Vladislav Mihov <vlad.slav.vo.wo@gmail.com>
2021-01-09 13:48:21 +0200

    Finally fixed navbar!

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-const navResetOnResize = (nav, navClass, burger, burgerClass) => {
+const navResetOnResize = (nav, navClass, burger, burgerClass, navLinkFade) => {
     nav.classList.remove(navClass);
+    navLinkFade();
     burger.classList.remove(burgerClass);
 }
 
@@ -7,12 +8,12 @@ const navLinksFade = () => {
     const navLinks = document.querySelectorAll('.nav-links li');
     
     navLinks.forEach((link, index) => {
-        if (link.style.animation) {
-            link.style.animation = '';
-        }
-        else {
+        // if (link.style.animation) {
+        //     link.style.animation = '';
+        //     TODO: decide if we want other animation for nav links from regular navbar
+        // }
+        // else {
             link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + 0.5}s`;
-        }
     });
 }
 
@@ -33,8 +34,9 @@ const navToggle = () => {
             navResetOnResize(nav, 'nav-active', burger, 'toggle', navLinksFade);
         }
     });
-    // navResetOnResize(nav, 'nav-active', burger, 'toggle', navLinksFade);
-
+    if (mediaQuery.matches){
+        navResetOnResize(nav, 'nav-active', burger, 'toggle', navLinksFade);
+    }
 }
 
 const app = () => {
