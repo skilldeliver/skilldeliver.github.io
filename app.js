a04755c8d7b0c420b6c28a4c95be3785c22215b8
Vladislav Mihov <vlad.slav.vo.wo@gmail.com>
2021-01-10 19:56:25 +0200

    Add responsive design queries.

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ const navResetOnResize = (nav, navClass, burger, burgerClass, navLinkFade) => {
 const navLinksFade = () => {
     if (!onResize){
         const navLinks = document.querySelectorAll('.nav-links li');
-        
+
         navLinks.forEach((link, index) => {
             if (link.style.animation) {
-                link.style.animation = '';
+                link.style.animation = ``;
             }
             else {
                 link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + 0.5}s`;
