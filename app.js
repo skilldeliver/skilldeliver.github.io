1d55cc6fa366e7c73ccece19a46c2e933fc7ff45
Vladislav Mihov <vlad.slav.vo.wo@gmail.com>
2021-01-10 13:06:16 +0200

    Add navbar theme and fixed functionality.

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
 const navResetOnResize = (nav, navClass, burger, burgerClass, navLinkFade) => {
+    onResize = true;
     nav.classList.remove(navClass);
-    navLinkFade();
+    navLinkFade(onResize);
     burger.classList.remove(burgerClass);
 }
 
 const navLinksFade = () => {
-    const navLinks = document.querySelectorAll('.nav-links li');
-    
-    navLinks.forEach((link, index) => {
-        // if (link.style.animation) {
-        //     link.style.animation = '';
-        //     TODO: decide if we want other animation for nav links from regular navbar
-        // }
-        // else {
-            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + 0.5}s`;
-    });
+    if (!onResize){
+        const navLinks = document.querySelectorAll('.nav-links li');
+        
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            }
+            else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + 0.5}s`;
+            }
+        });
+    }
 }
 
 
@@ -22,21 +25,25 @@ const navToggle = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
     const mediaQuery = window.matchMedia('(min-width: 768px)');
+    const body = document.querySelector('body');
+    onResize = false;
 
     burger.addEventListener('click', () => {
+        body.classList.toggle('hide');
         nav.classList.toggle('nav-active');
-        navLinksFade();
+        navLinksFade(onResize);
         // Burger animation
         burger.classList.toggle('toggle');
     });
     mediaQuery.addEventListener("change", () => {
         if (mediaQuery.matches){
+            body.classList.remove('hide');
             navResetOnResize(nav, 'nav-active', burger, 'toggle', navLinksFade);
         }
     });
-    if (mediaQuery.matches){
-        navResetOnResize(nav, 'nav-active', burger, 'toggle', navLinksFade);
-    }
+    // if (mediaQuery.matches){
+    //     navResetOnResize(nav, 'nav-active', burger, 'toggle', navLinksFade);
+    // }
 }
 
 const app = () => {
