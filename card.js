52e9a3aea3b624fd994cd231f7c41a03a40fdf4d
Vladislav Mihov <vlad.slav.vo.wo@gmail.com>
2021-01-18 12:45:02 +0200

    Add animations.

--- card.js
+++ card.js
@@ -3,3 +3,22 @@ $('.card').tilt({
     glare: true,
     maxGlare: .7,
 })
+
+$(window).scroll(function() {
+    $('.text').each(function(){
+    var imagePos = $(this).offset().top;
+
+    var topOfWindow = $(window).scrollTop();
+        if (imagePos < topOfWindow+400) {
+            $(this).addClass("slideExpandUp");
+        }
+    });
+    $('.about-personal-photo').each(function(){
+        var imagePos = $(this).offset().top;
+    
+        var topOfWindow = $(window).scrollTop();
+            if (imagePos < topOfWindow+400) {
+                $(this).addClass("bigEntrance");
+            }
+        });
+});
