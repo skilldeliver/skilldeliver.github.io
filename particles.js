a0529f1ec9bed687cc74d3ab0075b723b65261d9
skilldeliver <vladislav.d.mihov@gmail.com>
2021-02-02 22:58:01 +0200

    Fix alignment of bio sections.

--- particles.js
+++ particles.js
@@ -1,3 +1,3 @@
-particlesJS.load('particles-js', 'particlesjs-config.json', function() {
+particlesJS.load('experience', 'particlesjs-config.json', function() {
     console.log('callback - particles.js config loaded');
   });
