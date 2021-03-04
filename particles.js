9812419ab1085f8dc09487c04de08886de7e9232
skilldeliver <vladislav.d.mihov@gmail.com>
2021-03-04 15:14:50 +0200

    Remove particles from now from the bio.html codebase.

--- particles.js
+++ particles.js
@@ -1,3 +1,3 @@
-particlesJS.load('experience', 'particlesjs-config.json', function() {
+particlesJS.load('particles-experience', 'particlesjs-config.json', function() {
     console.log('callback - particles.js config loaded');
   });
